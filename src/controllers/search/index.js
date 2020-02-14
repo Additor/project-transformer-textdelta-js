11ba1d252c87af9a4432a4a4d3c5dd7c56e9906b
InJun Choi <000xor1@gmail.com>
2020-02-14 21:27:14 +0900

    CLN: 엘라스틱서치 검색, 카운트 쿼리 분리

--- src/controllers/search/index.js
+++ src/controllers/search/index.js
@@ -1,20 +1,13 @@
 import AppError from '../../utils/appError';
-import client from '../../database/db';
+import { search, count } from '../../database/db';
 
 const createDoc = docs => {
-  //elasticsearch에서 검색한 결과를 바탕으로 필요한 정보만 추출하여 새로운 object로 만들기
-
-  const docCount = docs.length;
-  let result = [];
-
-  for (let i = 0; i < docCount; i++) {
-    result.push({
-      ws_id: docs[i]['_source']['ws_id'],
-      note_id: docs[i]['_source']['note_id'],
-      text: docs[i]['_source']['text'],
-    });
-  }
-  return result;
+  //elasticsearch에서 검색한 결과를 바탕으로 필요한 정보만 추출하여 새로운 object로 만든다.
+  return docs.map(doc => ({
+    ws_id: doc['_source']['ws_id'],
+    note_id: doc['_source']['note_id'],
+    text: doc['_source']['text'],
+  }));
 };
 
 export const searchCtrl = async (req, res, next) => {
@@ -60,7 +53,6 @@ export const searchCtrl = async (req, res, next) => {
     );
   }
 
-  //ws_id, q, page Type 검사
   if (page < 1) {
     return next(
       new AppError(400, 'SE02', 'Invalid page value (부적절한 page 값입니다.)'),
@@ -68,35 +60,13 @@ export const searchCtrl = async (req, res, next) => {
       res,
       next,
     );
-  } else {
-    page = page - 1;
   }
 
   //Elasticsearch Total Count
-  //Work space에서 검색어와 일치하는 Documents 총 갯수
-  //Pagenation 작업을 하기 위해서 필요
   try {
-    elsResponse = await client.count({
-      index: 'document',
-      body: {
-        query: {
-          bool: {
-            must: {
-              match: {
-                text: q,
-              },
-            },
-            filter: {
-              term: {
-                ws_id: ws_id,
-              },
-            },
-          },
-        },
-      },
-    });
+    elsResponse = await count({ ws_id, q });
     totalCount = elsResponse['count'];
-  } catch (error) {
+  } catch (err) {
     return next(
       new AppError(500, 'SE98', 'ElasticSearch Error (엘라스틱서치 에러)'),
       req,
@@ -116,30 +86,19 @@ export const searchCtrl = async (req, res, next) => {
     totalPage = Math.ceil(totalCount / size);
   }
 
-  //elasticsearch 검색 (검색하는 쿼리 분리 필요)
+  if (page > totalPage) {
+    return next(
+      new AppError(400, 'SE02', 'Invalid page value (부적절한 page 값입니다.)'),
+      req,
+      res,
+      next,
+    );
+  }
+
+  //elasticsearch 검색
   try {
-    elsResponse = await client.search({
-      index: 'document',
-      from: page,
-      size: size,
-      body: {
-        query: {
-          bool: {
-            must: {
-              match: {
-                text: q,
-              },
-            },
-            filter: {
-              term: {
-                ws_id: ws_id,
-              },
-            },
-          },
-        },
-      },
-    });
-  } catch (error) {
+    elsResponse = await search({ page, size, q, ws_id });
+  } catch (err) {
     return next(
       new AppError(500, 'SE98', 'ElasticSearch Error (엘라스틱서치 에러)'),
       req,
